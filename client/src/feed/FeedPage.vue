396a4a8a1e51e99d7bb28964a5fb4d83eea5a0f5
Ohad Chaet <ohadch9518@gmail.com>
2020-03-06 17:23:21 +0200

    Fetching the list of posts

---
+++ client/src/feed/FeedPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul>
+        <Post v-for="post in posts" :post="post" :key="post.id" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+import Post from "./Post";
+
+export default {
+  name: "FeedPage",
+  components: {
+      Post
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    posts: gql`
+      query GetAllPosts {
+        posts {
+          id
+          title
+          body
+          createdAt
+          updatedAt
+        }
+      }
+    `
+  }
+};
+</script>
+
+<style>
+ul {
+    list-style: none;
+    padding: 10px;
+}
+</style>
---
+++ client/src/feed/Post.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    post: Object
+  }
+};
+</script>
+
+<style>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  background: white;
+  padding: 10px;
+  margin: 10px;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
